435f6f35542f7c1a6a196cb113e13b056becfbb5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-02 14:44:48 +0300

    pages setup

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+    return ( 
+        <>
+            <form>
+                <h3>Login to your Account</h3>
+            </form>
+        </>
+     );
+}
+ 
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+const Register = () => {
+    return ( 
+        <>
+            <form>
+                <h3>Create New Account</h3>
+            </form>
+        </>
+     );
+}
+ 
+export default Register;
