c0e236b1e31d4357c6b27b0c1a18ea3cd97b4243
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-02 15:05:10 +0300

    login form created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,21 @@ const Login = () => {
         <>
             <form>
                 <h3>Login to your Account</h3>
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="text"
+                    placeholder="Enter your Email"
+                    required
+                />
+
+                <label htmlFor="password">Password</label>
+                <input 
+                    type="password"
+                    placeholder="Enter Password"
+                    required
+                />
+
+                <button type="submit">Login</button>
             </form>
         </>
      );
