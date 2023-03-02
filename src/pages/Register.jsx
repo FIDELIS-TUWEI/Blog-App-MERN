e49ab1501dde14410627df51bb2af01c042283f0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-02 15:02:29 +0300

    register form state

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 const Register = () => {
+    //useState
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    // handleRegister
+    const register = (e) => {
+        e.preventDefault();
+    }
     return ( 
         <>
-            <form>
+            <form onSubmit={register}>
                 <h3>Create New Account</h3>
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="text"
+                    placeholder="Enter Your Email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <label htmlFor="password">Password</label>
+                <input 
+                    type="password"
+                    placeholder="Enter Password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button>Register</button>
             </form>
         </>
      );
