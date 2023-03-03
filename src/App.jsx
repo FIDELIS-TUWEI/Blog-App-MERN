2c5197bcd0198347f7cf6198ce7cabbce4b9ee8e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-03 09:59:38 +0300

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
 import Form from './components/common/Form'
+import Home from './pages/Home';
+
+// routes
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+// function for router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='login' element={<Form title="Login" />} />
+      <Route path='register' element={<Form title="Register" />} />
+    </Route>
+  )
+)
 
 function App() {
 
   return (
     <div className="App">
-      <Form />
+      <RouterProvider router={router} />
     </div>
   )
 }
