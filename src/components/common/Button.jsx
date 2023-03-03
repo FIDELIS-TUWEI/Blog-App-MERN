28976eb13e98423acd4fc2fb749ac3caa92000a9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-03 08:42:59 +0300

    button MUI created

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+const BasicButtons = () => {
+    return ( 
+        <Button variant="contained">Login</Button>
+    );
+}
+ 
+export default BasicButtons;
--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -2,17 +2,23 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 const BasicTextFields = () => {
-    return ( 
-        <Box
-            component="form"
-            sx={{
-                '& > :not(style)' : {m: 1, width: '25ch'},
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Box>
+    return (
+        <div>
+            <div className="heading__container">
+                <h3>Login to your Account</h3>
+            </div> 
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)' : {m: 1, width: '25ch'},
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField id="outlined-basic" label="Enter your Email" variant="outlined" />
+                <TextField id="password" label="Enter your password" variant="outlined" />
+            </Box>
+        </div>
     );
 }
  
