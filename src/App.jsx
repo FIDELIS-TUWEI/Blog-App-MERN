113745ece9ce9bce0636b0c017f0f85af4809c5d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-04 08:41:44 +0300

    useState passed as props to form

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
+  //useState
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path='login' element={<Form title="Login" />} />
-          <Route path='register' element={<Form title="Register" />} />
+          <Route 
+            path='login' 
+            element={
+              <Form 
+                title="Login" 
+                setEmail={setEmail}
+                setPassword={setPassword}
+              />} 
+          />
+
+          <Route 
+            path='register' 
+            element={
+              <Form 
+                title="Register"
+                setEmail={setEmail}
+                setPassword={setPassword} 
+              />} 
+          />
         </Routes>
       </div>
     </Router>
