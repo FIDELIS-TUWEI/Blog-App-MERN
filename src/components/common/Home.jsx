1b9f4b5b55f767ae1dc7e002b01808762d577b24
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-04 11:16:29 +0300

    useEffect to check sessionToken

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+    // useNavigate
+    const navigate = useNavigate()
+
+    // useEffect for private routes
+    useEffect(() => {
+        //check if token is valid go to home
+        let authToken = sessionStorage.getItem('Auth Token')
+        if (authToken) {
+            navigate('/home')
+        }
+
+        // if token not valid return to login
+        if (!authToken) {
+            navigate('/login')
+        }
+    }, [])
     return ( 
         <>
             <h3>Home page</h3>
