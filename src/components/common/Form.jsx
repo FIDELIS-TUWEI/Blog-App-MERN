3d2e7e91a54bf40bd6068f00ad25c60cf0dbd0b2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-22 21:53:08 +0300

    Grid MUI sx

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -12,6 +12,21 @@ const BasicTextFields = ({title, setEmail, setPassword, handleAction}) => {
         <ThemeProvider theme={theme}>
             <Grid container component="main" sx={{height: '100vh'}}>
                 <CssBaseline />
+
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) => 
+                            t.palette.mode = 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        ackgroundPosition: 'center',
+                    }}
+                />
                 <Box
                     component="form"
                     sx={{
