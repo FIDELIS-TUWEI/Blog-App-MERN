dccef95f9641c26747d4834918cb751527ec0045
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-22 21:44:21 +0300

    Grid container added

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -9,31 +9,30 @@ const BasicTextFields = ({title, setEmail, setPassword, handleAction}) => {
     const theme = createTheme()
     return (
         <ThemeProvider theme={theme}>
-            <div className="heading__container">
-                <h3>{title} Account</h3>
-            </div> 
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)' : {m: 1, width: '25ch'},
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField 
-                    id="outlined-basic" 
-                    label="Enter your Email" 
-                    variant="outlined"
-                    onChange={(e) => setEmail(e.target.value)} 
-                />
-                <TextField 
-                    id="password" 
-                    label="Enter your password" 
-                    variant="outlined" 
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </Box>
-            <Button title={title} handleAction={handleAction}/>
+            <Grid>
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)' : {m: 1, width: '25ch'},
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField 
+                        id="outlined-basic" 
+                        label="Enter your Email" 
+                        variant="outlined"
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                    <TextField 
+                        id="password" 
+                        label="Enter your password" 
+                        variant="outlined" 
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Box>
+                <Button title={title} handleAction={handleAction}/>
+            </Grid>
         </ThemeProvider>
     );
 }
