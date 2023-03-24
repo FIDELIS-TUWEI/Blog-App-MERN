57d03e47be61733396a9bda3724528d9db742429
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-24 16:29:00 +0300

    Grid background image

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -15,7 +15,32 @@ const theme = createTheme()
 const Form = ({title, setEmail, setPassword, handleAction}) => {
     return ( 
         <ThemeProvider theme={theme}>
+            <Grid
+                container
+                component="main"
+                sx={{
+                    height: '100vh'
+                }}
+            >
+                <CssBaseline />
 
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50]: t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+
+                <Grid></Grid>
+            </Grid>
         </ThemeProvider>
      );
 }
