7e5d827cd98e1e6b69b5ce4b5a7faef7ad8c46ab
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-24 12:34:44 +0300

    form imports

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,3 +1,14 @@
+import Grid from "@mui/material/Grid";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import LockOutlinedIcon from '@mui/material/LockOutlined'
+
 const Form = () => {
     return ( 
         <></>
