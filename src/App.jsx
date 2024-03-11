5a8dc28feef82bd2dc0ada6032573a1f4f584f96
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-11 14:35:59 +0300

    App imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
+import Header, { ResponsiveMenu } from "./components/Header";
 import Home from "./components/Home";
 import Services from "./components/Services";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
       <>
+        <ResponsiveMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <Services />
         <Footer />
