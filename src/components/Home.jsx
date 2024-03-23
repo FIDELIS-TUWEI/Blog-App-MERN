c26925032ecc7dc5e0d22d9055be5f3a4005fb4c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-23 12:42:09 +0300

    project restructure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import "../styles/home.css";
-import Background from "../assets/background1.jpg";
+import Background from "../assets/bg1.jpg";
 
 const Home = () => {
   return (
     <section id="home" className="home">
         <h1 className="home--title"></h1>
         <div className="home--image">
-          <img src={Background} alt="background image" />
+          <img src={Background} alt="" />
         </div>
     </section>
   )
