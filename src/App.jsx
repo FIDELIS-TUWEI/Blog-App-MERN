7e44f5cf149bc669d1e85b48e4cd14926ac7c23a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-23 13:21:45 +0300

    import path updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-import Footer from "./components/Footer";
-import Header, { ResponsiveMenu } from "./components/Header";
-import Home from "./components/Home";
-import Services from "./components/Services";
+import Header, { ResponsiveMenu } from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
