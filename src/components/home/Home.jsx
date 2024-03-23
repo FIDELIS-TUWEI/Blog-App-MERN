701b8238c1fcdbf2bd29140a850e4ce825bdfcc4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-23 16:18:37 +0300

    Home UI design

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,33 +1,57 @@
 import "./home.css";
 import Background from "../../assets/bg1.jpg";
 import Banner from "../../assets/Banner.png";
+import SuccessBanner from "../../assets/about2.jpg";
+
+const HeroLanding = () => (
+  <>
+    <div className="home--image">
+      <img src={Background} alt="" />
+    </div>
+        
+    <div className="home--info">
+      <div className="banner--image">
+        <img src={Banner} alt="" />
+      </div>
+
+      <div className="home--content">
+        <h2 className="home--slogan">Leading Guest entertainment and communication solutions in Africa</h2>
+        <p className="home--text">
+          In partnership with leading global ICT companies we design and deliver highly <br /> innovative solutions that add value to our clients’ service. <br />
+          We have a strong commitment to good customer service delivered through our <br /> support offices in Nairobi.
+        </p>
+
+        <div className="home--btn">
+          <button>More</button>
+          <button>Work with Us</button>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+const HeroContent = () => (
+  <div className="success--container">
+      <img src={SuccessBanner} alt="" />
+
+      <div className="success--content">
+        <h3>10+ years of success stories across Africa</h3>
+        <small>
+          Providing our clients with premium, Innovative solutions and enhanced overall customer experience.
+        </small>
+
+        <br />
+        <button>Success Stories</button>
+      </div>
+      
+  </div>
+)
 
 const Home = () => {
   return (
     <section id="home" className="home">
-        <div className="home--image">
-          <img src={Background} alt="" />
-        </div>
-        
-        <div className="home--info">
-          <div className="banner--image">
-            <img src={Banner} alt="" />
-          </div>
-
-          <div className="home--content">
-            <h2 className="home--slogan">Leading Guest entertainment and communication solutions in Africa</h2>
-            <p className="home--text">
-              In partnership with leading global ICT companies we design and deliver highly <br /> innovative solutions that add value to our clients’ service. <br />
-              We have a strong commitment to good customer service delivered through our <br /> support offices in Nairobi.
-            </p>
-
-            <div className="home--btn">
-              <button>More</button>
-              <button>Work with Us</button>
-            </div>
-            
-          </div>
-        </div>
+      <HeroLanding />
+      <HeroContent />
     </section>
   )
 }
