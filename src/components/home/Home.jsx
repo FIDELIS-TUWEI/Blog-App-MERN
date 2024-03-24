4beba3be5d468771539dcb7c1c7b86af3e5a02d4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-24 05:40:09 +0300

    Home UI design

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -51,9 +51,81 @@ const HeroContent = () => (
 );
 
 const HeroSolutions = () => (
-  <div className="solutions--container">
+  <>
+    <div className="solutions--container"></div>
+    <div className="solutions--content">
       <h4>Solutions</h4>
-  </div>
+      <h2>Expertise</h2>
+    </div>
+    <div className="solutions--card"></div>
+    <div className="card--container">
+      <article className="card--layout">
+        <div className="card--title">
+          <h5>IPTV</h5>
+        </div>
+
+        <p>
+          IPTV is the new generation of High Definition (HD) digital interactive television systems for hotels, communities and ships
+        </p>
+        <button className="start--btn">Start Today</button>
+      </article>
+
+      <article className="card--layout">
+        <div className="card--title">
+          <h5>HSIA</h5>
+        </div>
+
+        <p>
+          For today’s increasingly mobile world, a guest’s main focus is around High-Speed Internet Access (HSIA). The PTC HSIA platform is an ideal solution to connect the guest’s devices
+        </p>
+        <button className="story--btn">Learn More</button>
+      </article>
+
+      <article className="card--layout">
+        <div className="card--title">
+          <h5>Digital Signage</h5>
+        </div>
+
+        <p>
+          PTC offers Digital Signage solutions that relay eye-catching messages and promotions. Digital signage is one of the most effective communication mediums in the market
+        </p>
+        <button className="story--btn">Learn More</button>
+      </article>
+
+      <article className="card--layout">
+        <div className="card--title">
+          <h5>CCTV</h5>
+        </div>
+
+        <p>
+          We are installers of CCTV cameras offering the best high end CCTV equipment. Our cameras offer feature for remote viewing via mobile phone, tablet or PC
+        </p>
+        <button className="start--btn">Learn More</button>
+      </article>
+
+      <article className="card--layout">
+        <div className="card--title">
+          <h5>Hotel PMS</h5>
+        </div>
+
+        <p>
+          Our Hotel Management software (AI-Powered) is an ideal application for hotel businesses. It is an integrated software solution that helps you manage the hotel's operations like booking, management, organization, and other related procedures
+        </p>
+        <button className="story--btn">Learn More</button>
+      </article>
+
+      <article className="card--layout">
+        <div className="card--title">
+          <h5>Training</h5>
+        </div>
+
+        <p>
+        At PTC, training forms a fundamental part of our total solution approach. We have qualified training personnel available to ensure that our clients are properly and efficiently trained in the operation of any equipment and software, which we provide
+        </p>
+        <button className="story--btn">Learn More</button>
+      </article>
+    </div>
+  </>
 )
 
 const Home = () => {
