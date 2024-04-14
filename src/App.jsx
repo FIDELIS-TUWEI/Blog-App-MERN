39bfbb6b6b9119cccd24885ed819951e37cf493b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-14 13:21:23 +0300

    Dynamically import components logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import {
+  Navbar, Hero, Analytics
+} from "./components/index"
 
 const App = () => (
 <div>
   <Navbar />
   <Hero />
+  <Analytics />
 </div>
 );
 
