489226f2357e59dc15dab017e82fe3151d8b5b56
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-16 12:41:33 +0300

    assests index file

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import double from "../assets/double.png";
+import laptop from "../assets/laptop.jpg";
+import single from "../assets/single.png";
+import triple from "../assets/triple.png";
+
+export {
+    double, laptop, single, triple
+};
