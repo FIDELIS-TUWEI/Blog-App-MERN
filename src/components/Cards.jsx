a7797d4e9cd53ac60facc947dcf90ea860c2068b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-22 10:47:56 +0300

    id property added for easy navigation on responsive menu

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import { double, single, triple } from "../assets";
 
 const Cards = () => {
   return (
-    <div className="w-full py-[10rem] bg-white">
+    <div className="w-full py-[10rem] bg-white" id="resources">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img src={single} alt="/" className="w-20 mx-auto mt-[-3rem] bg-white" />
